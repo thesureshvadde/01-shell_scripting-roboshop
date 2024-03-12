f6fa5afefb96a6a181d005691b00b39a1148b442
thesureshvadde <vaddesureshvarma@gmail.com>
2024-03-12 14:21:29 +0530

    roboshop

--- common.sh
+++ common.sh
@@ -1,15 +1,36 @@
-
+logfile=/tmp/roboshop.log
 
 print_head() {
-    echo $1
+    echo -e "\e[36m $1 \e[0m"
 }
 
 status_check() {
     if [ $? -eq 0 ]
     then
-        echo "success"
+        echo -e "\e[32m success \e[0m"
     else
-        echo "failure"
+        echo -e "\e[31m failure \e[0m"
+        exit 1
     fi
 }
 
+userstatus(){
+    id roboshop &>> ${logfile}
+    if [ $? -eq 0 ]
+    then
+        echo -e "\e[32m user already exist \e[0m"
+    else
+        useradd roboshop &>> ${logfile}   
+        status $?
+    fi
+}
+
+appstatus(){
+    cd /app &>> ${logfile}
+    if [ $? -eq 0 ]
+    then
+        echo -e "\e[32m /app already exist \e[0m" 
+    else
+        mkdir /app &>> ${logfile}
+    fi
+}
